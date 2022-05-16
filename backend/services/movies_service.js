bdf25dcb4bfb0aa985e45fef81b4980807b2c8eb
ashaman98 <karamyan.k@gmail.com>
2022-05-16 17:23:30 +0400

    user list functionality added

--- backend/services/movies_service.js
+++ backend/services/movies_service.js
@@ -1,103 +1,115 @@
 const { default: axios } = require("axios");
-const { NotFound, Conflict, InternalServerError } = require("../middlewares/errorHandler");
+const {
+  NotFound,
+  Conflict,
+  InternalServerError,
+} = require("../middlewares/errorHandler");
 const Log = require("../models/log");
 const Movie = require("../models/movie");
 const { startSession } = require("mongoose");
 const User = require("../models/user");
 
-async function fetchMovie(title){
-    const apiUrl = `http://www.omdbapi.com/?apikey=ec35c852&t=${title}`;
-    const res = await axios.get(apiUrl);
-    
-    if(res.data.Error){
-        throw new NotFound("Movie not found")
-    }
+async function fetchMovie(title) {
+  const apiUrl = `http://www.omdbapi.com/?apikey=ec35c852&t=${title}`;
+  const res = await axios.get(apiUrl);
 
-    const {Title,Year,Director,imdbID} = res.data
-    return {Title,Year,Director,imdbID};
+  if (res.data.Error) {
+    throw new NotFound("Movie not found");
+  }
+
+  const { Title, Year, Director, imdbID } = res.data;
+  return { Title, Year, Director, imdbID };
 }
 
-async function getMovie(title){
-    const movie = await fetchMovie(title);
-    return movie;
+async function getMovie(title) {
+  const movie = await fetchMovie(title);
+  return movie;
 }
 
-async function logMovie(data, user, status){
-    const movieExists = await Movie.findOne({imdb_id: data.imdbID})
+async function logMovie(data, user, status) {
+  const movieExists = await Movie.findOne({ imdb_id: data.imdbID });
 
-    if(movieExists){
-
-        const exists = await Log.findOne({
-            user_id: user._id,
-            movie_id: movieExists._id,
-            status: status
-        })
-        if(exists){
-            throw new Conflict("Movie Already logged with this status")
-        }
-        const log = await Log.create({
-            user_id: user._id,
-            movie_id: movieExists._id,
-            status: status
-        })
-
-        return log;
+  if (movieExists) {
+    const exists = await Log.findOne({
+      user_id: user._id,
+      movie_id: movieExists._id,
+      status: status,
+    });
+    if (exists) {
+      throw new Conflict("Movie Already logged with this status");
     }
-
-    const movie = await Movie.create({
-        title: data.Title,
-        director: data.Director,
-        release_date: data.Year,
-        imdb_id: data.imdbID
-    })
-
     const log = await Log.create({
-        user_id: user._id,
-        movie_id: movie._id,
-        status: status
-    })
+      user_id: user._id,
+      movie_id: movieExists._id,
+      status: status,
+    });
 
     return log;
+  }
 
+  const movie = await Movie.create({
+    title: data.Title,
+    director: data.Director,
+    release_date: data.Year,
+    imdb_id: data.imdbID,
+  });
+
+  const log = await Log.create({
+    user_id: user._id,
+    movie_id: movie._id,
+    status: status,
+  });
+
+  return log;
 }
 
-async function getList(user_id){
-    console.log("Commencing retrieval of user's movies")
-    const user = await User.findById(user_id);
-    if (!user) throw new NotFound("user not found");
-    
-    const session = await startSession()
+async function getList(user_id) {
+  console.log("Commencing retrieval of user's movies");
+  const user = await User.findById(user_id);
+  if (!user) throw new NotFound("user not found");
 
-    try{
-        session.startTransaction()
-        const userLogs = await Log.find({user_id: user_id},null,{session})
-        if(!userLogs) throw new NotFound("User has no entries")
-        console.log(userLogs)
+  try {
+    const userLogs = await Log.find({ user_id: user_id });
+    if (!userLogs) throw new NotFound("User has no entries");
+    console.log(userLogs);
 
-        console.log("getting the movie list")
-        const movieList = []
+    console.log("getting the movie list");
 
-        for (const elem of userLogs){
-            const data = await Movie.findOne({_id: elem.movie_id}, null,{session})
-            //data['status'] = elem.status                                 TODO: add the status to entry
-                                                                        //ERROR:Cannot set properties of null (setting 'status')
-            movieList.push(data)
-        }
-    
-        await session.commitTransaction();
-        session.endSession();
-        console.log("Succesfully retrieved data");
+    const movieList = await Movie.aggregate([
+      {
+        $match: {
+          _id: {
+            $in: userLogs.map((log) => log.movie_id),
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "logs",
+          localField: "_id",
+          foreignField: "movie_id",
+          as: "logs",
+        },
+      },
+      {
+        $addFields: {
+          status: {
+            $arrayElemAt: ["$logs.status", 0],
+          },
+        },
+      },
+      {
+        $project: {
+          logs: 0,
+        },
+      },
+    ]);
+    return movieList;
+  } catch (err) {
+    console.log(err.message);
 
-        return movieList;
-
-    }catch(err){
-        console.log("Transaction failed, reverting changes");
-        await session.abortTransaction();
-        session.endSession();
-        console.log(err.message);
-        
-        throw new InternalServerError("Internal error")
-    }
+    throw new InternalServerError("Internal error");
+  }
 }
 
-module.exports = {getMovie, logMovie, getList}
+module.exports = { getMovie, logMovie, getList };
